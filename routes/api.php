b5c85e007944cba87a5918e5fa98a57819eaecba
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-10-27 01:02:12 -0600

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(CategoryController::class)->group(function(){
+    Route::post('nuevaCategoria', 'newCategory');
+    Route::get('categorias', 'getCategories');
+    Route::get('categoria/{id}', 'getCategoryById');
+    Route::delete('eliminarCategoria/{id}', 'deleteCategoryById');
+});
+
+Route::controller(ProductController::class)-> group(function(){
+    Route::post('nuevoProducto', 'newProduct');
+    Route::get('productos', 'getProducts');
+    Route::get('producto/{id}', 'getProductById');
+    Route::delete('eliminarProducto/{id}', 'deleteProductById');
+    Route::post('actualizarProducto/{id}', 'updateProductById');
+});
