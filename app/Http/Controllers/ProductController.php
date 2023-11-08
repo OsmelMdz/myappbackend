4f32d3dac401467993f1b2e81003cd1d9bd63941
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-11-08 05:18:01 -0600

    actualizar

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,6 @@ class ProductController extends Controller
 
     public function newProduct(Request $request)
     {
-
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:50',
             'price' => 'required|numeric',
@@ -37,11 +36,9 @@ class ProductController extends Controller
             'category_id' => 'required',
             'image' => 'required|image|max:2048',
         ]);
-
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-
         $rutaArchivoImg = $request->file('image')->store('public/imgproductos');
         $producto = Product::create([
             'name' => $request->name,
@@ -57,15 +54,61 @@ class ProductController extends Controller
         return response()->json(['producto' => $producto], 201);
     }
 
-     public function deleteProductById($id)
+    public function deleteProductById($id)
     {
         if ($product = Product::find($id)) {
-            // Actualizamos el campo state a 0
             $product->state = 0;
             $product->save();
             return response()->json(['success' => true]);
         } else {
             return response()->json(['error' => 'Producto no encontrado'], 404);
         }
+    }
+    public function updateProductById(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|max:50',
+            'price' => 'numeric',
+            'price_sale' => 'numeric',
+            'stock' => 'numeric|nullable',
+            'expired' => 'date|nullable',
+            'category_id' => 'numeric|nullable',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        if ($request->hasFile('image')) {
+            // Si se envía una nueva imagen, se elimina la imagen anterior y se almacena la nueva.
+            if ($product->image) {
+                Storage::delete($product->image);
+            }
+            $rutaArchivoImg = $request->file('image')->store('public/imgproductos');
+            $product->image = $rutaArchivoImg;
+        }
+        // Ahora, se actualizan los otros campos independientemente de si se envía una imagen o no.
+        if ($request->has('name')) {
+            $product->name = $request->input('name');
+        }
+        if ($request->has('category_id')) {
+            $product->category_id = $request->input('category_id');
+        }
+        if ($request->has('price')) {
+            $product->price = $request->input('price');
+        }
+        if ($request->has('price_sale')) {
+            $product->price_sale = $request->input('price_sale');
+        }
+        if ($request->has('stock')) {
+            $product->stock = $request->input('stock');
+        }
+        if ($request->has('expired')) {
+            $product->expired = $request->input('expired');
+        }
+        $product->save();
+        return response()->json(['message' => 'Producto actualizado con éxito'], 200);
     }
 }
